85c6035974966e6792d88efa214e09f740e82bd6
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-09-17 16:13:05 +0800

    Add project files.

---
+++ AspCoreWithAngular/Models/Student.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreWithAngular.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string Address { get; set; }
+
+        #region Student DataAccessLayer
+        string connectionString = "Put Your Connection string here";
+        //To View all employees details
+        public IEnumerable<Student> GetAll()
+        {
+            try
+            {
+                var listStudent = new List<Student>();
+                using (var con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetAllStudent", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        var employee = new Student();
+                        employee.StudentID = Convert.ToInt32(rdr["StudentID"]);
+                        employee.StudentName = rdr["StudentName"].ToString();
+                        employee.Address = rdr["Address"].ToString();
+                        listStudent.Add(employee);
+                    }
+                    con.Close();
+                }
+                return listStudent;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        //To Add new employee record 
+        public int Add(Student student)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spAddStudent", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@StudentID", student.StudentID);
+                    cmd.Parameters.AddWithValue("@StudentName", student.StudentName);
+                    cmd.Parameters.AddWithValue("@Address", student.Address);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        //To Update the records of a particluar employee
+        public int UpdateEmployee(Student student)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spUpdateStudent", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@StudentID", student.StudentID);
+                    cmd.Parameters.AddWithValue("@StudentName", student.StudentName);
+                    cmd.Parameters.AddWithValue("@Address", student.Address);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        //Get the details of a particular employee
+        public Student Get(int id)
+        {
+            try
+            {
+                var employee = new Student();
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string sqlQuery = "SELECT * FROM tblEmployee WHERE EmployeeID= " + id;
+                    SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        employee.StudentID = Convert.ToInt32(rdr["StudentID"]);
+                        employee.StudentName = rdr["StudentName"].ToString();
+                        employee.Address = rdr["Address"].ToString();
+                    }
+                }
+                return employee;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        //To Delete the record on a particular employee
+        public int DeleteEmployee(int id)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spDeleteStudent", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Student", id);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
